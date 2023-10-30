06661cc6b4f171aecd50fa735f7d849d228beacd
William Delmo <wdelmo5123@gmail.com>
2023-10-30 10:07:08 -0400

    added a person class docstring.

---
+++ uno.py
@@ -0,0 +1,9 @@
+"""A game of Uno against the computer."""
+
+class Person:
+    """Tracks the names of the player in Uno.
+
+        Attributes:
+        name(str): the person's name. 
+    """
+    
