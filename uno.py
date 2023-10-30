35cc0593f328de7329fa047668b1cb26593d3abb
Matthew Manik <zealandrio@gmail.com>
2023-10-30 10:21:53 -0400

    Modifed Person to Player class and created init method.

--- uno.py
+++ uno.py
@@ -1,9 +1,18 @@
 """A game of Uno against the computer."""
 
-class Person:
+class Player:
     """Tracks the names of the player in Uno.
 
         Attributes:
-        name(str): the person's name. 
+        name(str): the player's name. 
     """
+    
+    def __init__(self,name):
+        """ Initialize Player Object
+
+        Args:
+            name (_str): name of Player
+        """
+        
+        self.name = name
     
