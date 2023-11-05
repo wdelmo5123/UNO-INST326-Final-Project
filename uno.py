a642f9c8f51537e11a998e9ad35cec90021146dc
William Delmo <wdelmo5123@gmail.com>
2023-11-05 16:38:13 -0500

    Added an init method to ComputerPlayer class, added methods the init, skip, and reverse methods in the GameState method. Look at my code and let me know if y'all have any suggestions or if you want to edit it, change accordingly.

--- uno.py
+++ uno.py
@@ -23,3 +23,30 @@ class ComputerPlayer:
         names(str): the player's name.
         cards(str or list): the lists of cards that the computer will get cards from.
     """
+    def __init__(self,computer):
+        """ Initialize Player Object
+
+        Args:
+            name (_str): name of Player
+        """
+        
+        self.computer = computer
+
+class GameState:
+    
+    def __init__(self, player, card, hand, clockwise):
+        self.player = player
+        self.card = card
+        self.hand = hand
+        self.clockwise = clockwise
+        self.clockwise = True
+        
+    def skip_turn(self):
+        if self.card == "Skip":
+            self.player[i] += 1 
+            return print(f"{self.name} has been skipped!")
+        
+    def reverse_turn(self):
+        if self.card == "Reverse":
+            self.clockwise = False
+            return print(f"{self.name} has used a reverse card!")
